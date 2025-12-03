8db0a79d41ed7f860e8527fc298486c638be3d3e
abubekernuru <abubekernuru27@gmail.com>
2025-12-03 18:16:48 +0300

    create pages and routes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // Important for React/Vite setup
   ],
   theme: {
     extend: {},
