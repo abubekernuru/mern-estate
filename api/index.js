ac3df9483cfec66944f3ac1ae12ea0699a89f8ff
abubekernuru <abubekernuru27@gmail.com>
2025-12-05 20:20:39 +0300

    create a middleware and function to handle error

--- api/index.js
+++ api/index.js
@@ -20,3 +20,13 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 
+app.use((err, req, res, next)=> {
+    const error = new Error();
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal serval error!";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
