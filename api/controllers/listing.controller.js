17f0881fd84dfab10f2614d0e9a6d8041119f47f
abubekernuru <abubekernuru27@gmail.com>
2025-12-13 18:57:23 +0300

    add create listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,8 +1,14 @@
 const Listing = require('../models/listing.model.js')
+// const { errorHandler } = require('../utils/error.js')
 
 const createListing = async (req, res, next) => {
-    await new Listing(req.body);
-    res.status(200).json(Listing)
+    
+    try {
+        const listing = await new Listing(req.body);
+        res.status(200).json(listing)
+    } catch (error) {
+        next(error)
+    }
 }
 
 module.exports = { createListing };
