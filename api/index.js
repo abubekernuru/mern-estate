8e77b15831d119ee12d79d4c5dc833dc1a15a4ad
abubekernuru <abubekernuru27@gmail.com>
2025-12-28 13:23:35 +0300

    fix deployment issue

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/dist')));
   
-  app.get('/*', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, '/client/dist/index.html'));
   });
 }
